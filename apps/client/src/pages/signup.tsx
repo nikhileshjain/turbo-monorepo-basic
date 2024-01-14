ce5028430730863f671949392aea5d0a79ef3ca0
nikhileshjain <nikhilesh.jain07@gmail.com>
2024-01-14 20:39:44 +0530

    Created beautiful Signup page + Basic Backend Routing

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -2,6 +2,9 @@ import { Signup } from "@repo/ui/signup";
 
 export default function SignupPage() {
     return <div>
-        <Signup/>
+        <Signup onClick={async (username,password)=>{
+            // const res = await axios.post(`${BACKEND_URL}/admin/signup`,{username,password})
+            // console.log(res)
+        }}/>
     </div>
 }
